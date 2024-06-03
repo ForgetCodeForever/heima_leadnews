77d433e0ef9c18b19d1d8a2d82cbc64b73ee7452
ForgetCodeForever <161100913+ForgetCodeForever@users.noreply.github.com>
2024-06-03 16:47:40 +0800

    Redis实现延迟任务: 添加任务(addTaskToDb, addTaskToCache) 取消任务(removeTaskFromCache, updateDb) 消费任务(updateDb) zset中任务定时刷新至list 数据库中任务定时刷新至Redis(clearCache, addTaskToCache)

---
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/service/TaskService.java
@@ -0,0 +1,32 @@
+package com.heima.schedule.service;
+
+import com.heima.model.schedule.dtos.Task;
+
+/**
+ * 对外访问接口
+ */
+public interface TaskService {
+
+    /**
+     * 添加任务
+     * @param task   任务对象
+     * @return       任务id
+     */
+    long addTask(Task task) ;
+
+    /**
+     * 取消任务
+     * @param taskId        任务id
+     * @return              取消结果
+     */
+    boolean cancelTask(long taskId);
+
+    /**
+     * 按照类型和优先级来拉取任务  消费任务
+     * @param type
+     * @param priority
+     * @return
+     */
+    Task poll(int type, int priority);
+
+}
