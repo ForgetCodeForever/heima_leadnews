fb95452734e4e0a6642ef1bc25fa9d7588283bee
ForgetCodeForever <161100913+ForgetCodeForever@users.noreply.github.com>
2024-06-06 22:41:52 +0800

    spring集成kafka(传递string消息, 传递对象消息)

---
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/listener/HelloListener.java
@@ -0,0 +1,26 @@
+package com.heima.kafka.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.heima.kafka.pojo.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+public class HelloListener {
+
+    @KafkaListener(topics = "spring-topic")
+    public void onMessage(String message){
+        if(!StringUtils.isEmpty(message)){
+            System.out.println(message);
+        }
+    }
+
+    @KafkaListener(topics = "user-topic")
+    public void userMessage(String message){
+        if(!StringUtils.isEmpty(message)){
+            User user = JSON.parseObject(message, User.class);
+            System.out.println(user);
+        }
+    }
+}
